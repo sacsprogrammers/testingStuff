8b262e6c3b09328a40fad7a11bebc4f51306d7ac
AJ Amos <aamos22@student.sacs.nsw.edu.au>
2020-09-20 05:02:39 +0000

    New Test File

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import xlsxwriter
 def read_file(filename):
   return open(str(filename), 'r').read()
 
-
+"""
 json_query = read_file("GitHub_activity.graphql")
 url = 'https://api.github.com/graphql'
 headers = {"Authorization": "Bearer 1b5cf4050ba63218bf99ee2b1a36cbb7c7f31cca"}
@@ -16,10 +16,10 @@ r = requests.post(url, json={'query': json_query}, headers=headers)
 json_data = json.loads(r.text)
 AJ_data = json_data['data']['nodes']
 print(AJ_data)
-
+"""
 
 # Create a workbook and add a worksheet.
-workbook = xlsxwriter.Workbook('Expenses01.xlsx')
+workbook = xlsxwriter.Workbook('Expenses02.xlsx')
 worksheet = workbook.add_worksheet()
 
 # Some data we want to write to the worksheet.
